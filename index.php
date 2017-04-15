c141458c2186bfc45f8ff7db56dfa8ffa3dce047
Chukwunonso <offorma@gmail.com>
2017-04-15 16:47:30 +0100

    initial commit with login page

--- index.php
+++ index.php
@@ -11,16 +11,18 @@ include(header.php);
 
 
 <div class = "container">
-    <div class="wrapper">
-        <form action="" method="post" name="Login_Form" class="form-signin">
-            <h3 class="form-signin-heading">Welcome Back! Please Sign In</h3>
-            <hr class="colorgraph"><br>
+    <div class="row">
+        <div class="wrapper">
+            <form action="" method="post" name="Login_Form" class="form-signin">
+                <h3 class="form-signin-heading">Welcome Back! Please Sign In</h3>
+                <hr class="colorgraph"><br>
 
-            <input type="text" class="form-control" name="Username" placeholder="Username" required="" autofocus="" />
-            <input type="password" class="form-control" name="Password" placeholder="Password" required=""/>
+                <input type="text" class="form-control" name="Username" placeholder="Username" required="" autofocus="" />
+                <input type="password" class="form-control" name="Password" placeholder="Password" required=""/>
 
-            <button class="btn btn-lg btn-primary btn-block"  name="Submit" value="Login" type="Submit">Login</button>
-        </form>
+                <button class="btn btn-lg btn-primary btn-block"  name="Submit" value="Login" type="Submit">Login</button>
+            </form>
+        </div>
     </div>
 </div>
 
