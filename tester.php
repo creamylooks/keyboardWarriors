646e50a42a7e394606231a90e8a3eecad97acedd
creamylooks <f.o.malick@rgu.ac.uk>
2017-04-16 05:28:06 +0100

    Update tester.php

--- tester.php
+++ tester.php
@@ -1,27 +1,27 @@
-(<?php
+<?php
 /**
  * Created by PhpStorm.
  * User: offormachukwunonso
  * Date: 4/16/17
  * Time: 1:14 AM
  */
-<?php
+
 error_reporting(0);
 require ('db.php');
  
-$df = $_GET['password'];
-$nn = $_GET['username'];
-/*$name = $_POST["username"];
+//$df = $_GET['password'];
+//$nn = $_GET['username'];
+$name = $_POST["username"];
 $password = $_POST["password"];
-$email = $_POST["email"];
- */
+//$email = $_POST["email"];
+ 
 //$name = "sdf";
 //$password = "sdf";
 //$email = "sdf@r54";
  
 $sql=$link->query("INSERT INTO tester(tc)VALUES('$name')");
   $sql2=$link->query("INSERT INTO tester(tc)VALUES('$password')");
-  $sql3=$link->query("INSERT INTO tester(tc)VALUES('$email')");
+//  $sql3=$link->query("INSERT INTO tester(tc)VALUES('$email')");
   
 //$sql = "INSERT INTO `user_info` (`id`,`name`, `password`, `email`) VALUES (NULL, '".$name."', '".$password."', '".$email."');";
 
