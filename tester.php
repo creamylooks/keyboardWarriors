ca1365784cc3a807c72bf92b83abe6002ee81d2f
creamylooks <f.o.malick@rgu.ac.uk>
2017-04-16 04:03:25 +0100

    Update tester.php

--- tester.php
+++ tester.php
@@ -5,8 +5,24 @@
  * Date: 4/16/17
  * Time: 1:14 AM
  */
-require_once 'db.php';
-echo"{$_POST['tc']}";
-if(!empty($_POST['tc'])){
-$sql=$link->query("INSERT INTO tester(tc)VALUES('$tc')");
-}
+<?php
+error_reporting(0);
+require 'db.php';
+ 
+$name = $_POST["name"];
+$password = $_POST["password"];
+$email = $_POST["email"];
+ 
+//$name = "sdf";
+//$password = "sdf";
+//$email = "sdf@r54";
+ 
+$sql=$link->query("INSERT INTO tester(tc)VALUES('$name')");
+  $sql2=$link->query("INSERT INTO tester(tc)VALUES('$password')");
+  $sql3=$link->query("INSERT INTO tester(tc)VALUES('$email')");
+  
+//$sql = "INSERT INTO `user_info` (`id`,`name`, `password`, `email`) VALUES (NULL, '".$name."', '".$password."', '".$email."');";
+
+
+
+
