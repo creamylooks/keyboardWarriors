16d31d1f9c6d14041858fc1b8b5449f3573c1fb2
Chukwunonso <offorma@gmail.com>
2017-04-16 07:09:55 +0100

    initial commit with login page

--- header.php
+++ header.php
@@ -19,3 +19,22 @@
     <link href="https://cdn.datatables.net/1.10.13/css/jquery.dataTables.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
 </head>
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php">WarrantySafe</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="createstudent.php">Create Student</a></li>
+                <li><a href="createevent.php">Create Event</a></li>
+
+        </div>
+    </div>
+</nav>
