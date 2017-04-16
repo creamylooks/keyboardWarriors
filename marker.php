cbb4b7ff6f8a0ca1c89c5ac5616fa1ffda713d5d
creamylooks <f.o.malick@rgu.ac.uk>
2017-04-16 12:27:32 +0100

    Create marker.php

---
+++ marker.php
@@ -0,0 +1,54 @@
+<?php
+include('header.php');
+
+require_once 'db.php';
+  $cod = $_POST['studBCode'];
+?>
+  <body>
+  <div class="container">
+      <div class="row">
+          <?php
+          $sq1= "SELECT * FROM student WHERE student_ID = '$cod'";
+                $q1=mysqli_query($link, $sq1);
+            $rom = mysqli_fetch_assoc($q1);
+            ?>
+            <table class="table table-striped">
+              <thead>
+                <th> Student ID</th>
+                <th> First Name</th>
+                <th> Last Name</th>
+              </thead>
+              <tbody>
+                <td><?php echo $rom['student_ID']; ?></td>
+                <td><?php echo $rom['firstname'] ?></td>
+                <td><?php echo $rom['lastname'] ?></td>
+              <?php
+                  if($_POST['studBCode'] !=null){
+              while($_POST['studBCode'] !=$cod){
+                $sq2= "SELECT * FROM student WHERE student_ID = '$_POST['studBCode']'";
+                $q2=mysqli_query($link, $sq2);
+                $rome = mysqli_fetch_assoc($q2);
+                  echo   "<td>".'{$rome['student_ID']}'."<td>".
+                        "<td>".'{$rome['firstname']}'."<td>".
+                        "<td>".'{$rome['lastname']}'."<td>";
+                $cod = $_POST['studBCode'];
+              }
+            }
+              ?>
+
+              </body>
+
+
+
+            </table>
+
+
+
+        </div>
+        </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/js/bootstrap-select.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.4/js/bootstrap-datepicker.min.js"></script>
+        </body>
+        </html>
