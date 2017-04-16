ac10d1009dfe355889ad233905286d7d034ca556
Chukwunonso <offorma@gmail.com>
2017-04-16 08:53:45 +0100

    initial commit with login page

--- tester.php
+++ tester.php
@@ -11,20 +11,29 @@ require ('db.php');
  
 //$df = $_GET['password'];
 //$nn = $_GET['username'];
-$name = $_POST["username"];
-$password = $_POST["password"];
-//$email = $_POST["email"];
- 
-//$name = "sdf";
-//$password = "sdf";
-//$email = "sdf@r54";
+$id = $_POST["studBCode"];
+
  
 $sql=$link->query("INSERT INTO tester(tc)VALUES('$name')");
-  $sql2=$link->query("INSERT INTO tester(tc)VALUES('$password')");
+
 //  $sql3=$link->query("INSERT INTO tester(tc)VALUES('$email')");
   
 //$sql = "INSERT INTO `user_info` (`id`,`name`, `password`, `email`) VALUES (NULL, '".$name."', '".$password."', '".$email."');";
 
 
+$ans=$link->query("Select * From student where student_ID='$id'");
+$row=$ans->fetch_array();
+$firstname=$row['Firstname'];
+$lastname=$row['Lastname'];
+$image=$row['image_loc'];
+?>
+<img src='<?php echo "$image"?>' style="display: none">
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script>
+var img = $('img');
+alert(img.attr('src')); // foo.jpg
+var fullpath = window.location.host + img.attr('src');
+alert(fullpath); // http://example.com/foo.jpg
+</script>
 
