0aa20181272ab71312a09e307088b3c1f5562436
Chukwunonso <offorma@gmail.com>
2017-04-16 09:05:50 +0100

    initial commit with login page

--- tester.php
+++ tester.php
@@ -9,16 +9,9 @@
 error_reporting(0);
 require ('db.php');
  
-//$df = $_GET['password'];
-//$nn = $_GET['username'];
+
 $id = $_POST["studBCode"];
 
- 
-$sql=$link->query("INSERT INTO tester(tc)VALUES('$name')");
-
-//  $sql3=$link->query("INSERT INTO tester(tc)VALUES('$email')");
-  
-//$sql = "INSERT INTO `user_info` (`id`,`name`, `password`, `email`) VALUES (NULL, '".$name."', '".$password."', '".$email."');";
 
 
 $ans=$link->query("Select * From student where student_ID='$id'");
@@ -26,14 +19,9 @@ $row=$ans->fetch_array();
 $firstname=$row['Firstname'];
 $lastname=$row['Lastname'];
 $image=$row['image_loc'];
+echo "$image.':'.$firstname.':'.$lastname";
 ?>
-<img src='<?php echo "$image"?>' style="display: none">
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script>
-var img = $('img');
-alert(img.attr('src')); // foo.jpg
-var fullpath = window.location.host + img.attr('src');
-alert(fullpath); // http://example.com/foo.jpg
-</script>
+
+
 
